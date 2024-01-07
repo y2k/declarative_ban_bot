301e2176044ef4bd89f8b590c1a744b9dbc65d9e
y2k <itwisterlx@gmail.com>
2024-01-07 16:55:36 +0100

    add playground

--- src/main.clj
+++ src/main.clj
@@ -1,25 +1,20 @@
-(defn default_handle_request [env json next]
-  (fetch
-   (str "https://api.telegram.org/bot" env.TG_TOKEN "/sendMessage")
+
+(defn ^{:in fetch_received} fetch_json_received [request]
+  (.json! request))
+
+(defn ^{:in fetch_json_received} response_sended [json]
+  (fetch!
+   (str "https://api.telegram.org/bot" (read_env! "TG_TOKEN") "/sendMessage")
    {:method "POST"
     :headers {"Content-Type" "application/json"}
     :body
-    (.stringify
-     JSON
+    (JSON/stringify
      {:chat_id json.message.chat.id
       :link_preview_options {:url "https://youtu.be/fvwTZc-dxsM?si=kWJbOxk2UyLrvZdu"}
       :text "https://youtu.be/dQw4w9WgXcQ?si=V_MIImS5ydLbZaSu"})}))
 
-(defn fetch_handler [request env]
-  (->
-   (.json request)
-   (.then
-    (fn [json]
-      (->>
-       (.warn console "[LOG] Message not handled:\n" json) (fn [])
-       (default_handle_request env json))))
-   (.catch console.error)
-   (.then (fn [] (Response. "OK")))))
-
-(defn json_to_string [x] (.stringify JSON x null 2))
-(export-default {:fetch fetch_handler})
+(export-default {:fetch (fn [request env init_context]
+                          (->
+                           (runtime/consume_event (merge env init_context) :fetch_received request)
+                           (.catch console.error)
+                           (.then (fn [] (Response. "OK")))))})
