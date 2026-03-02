8fb82b593e45eb0f05f16555a8f5ecca8c91430c
y2k <itwisterlx@gmail.com>
2026-03-02 17:47:59 +0100

    refactor: unify logging functions and include user ID in log output

--- src/handler/report.clj
+++ src/handler/report.clj
@@ -15,19 +15,19 @@
 (defn- build-url [chat-name message-id]
   (str "https://t.me/" chat-name "/" message-id))
 
-(defn- notify-admin [reply-from-id is-spam chat-name reply-message-id reply-text]
+(defn- log-to-channel- [channel reply-from-id chat-name reply-message-id reply-text]
   (tg/send_message :sendMessage
-                   {:chat_id log-channel-important
+                   {:chat_id channel
                     :link_preview_options {:is_disabled true}
-                    :text (str "user: " reply-from-id
-                               "\nurl: " (build-url chat-name reply-message-id)
-                               "\ntext:\n" (.toString (Buffer/from reply-text) "base64"))}))
+                    :text (str "user: " reply-from-id "\n"
+                               "url: " (build-url chat-name reply-message-id) "\n"
+                               "text:\n" (.toString (Buffer/from reply-text) "base64"))}))
 
-(defn- log-to-channel [is-spam chat-name reply-message-id]
-  (tg/send_message :sendMessage
-                   {:chat_id log-channel
-                    :link_preview_options {:is_disabled true}
-                    :text (str "Bot invoked\nSpam: " is-spam "\nURL: " (build-url chat-name reply-message-id))}))
+(defn- notify [_ chat-name reply-message-id reply-from-id]
+  (log-to-channel- log-channel reply-from-id chat-name reply-message-id ""))
+
+(defn- notify-important [reply-from-id _ chat-name reply-message-id reply-text]
+  (log-to-channel- log-channel-important reply-from-id chat-name reply-message-id reply-text))
 
 (defn handle [update]
   (if-let [_ (report-command? update?.message?.text)
@@ -39,25 +39,25 @@
            reply-message-id update?.message?.reply_to_message?.message_id
            message-date update?.message?.reply_to_message?.date]
     (let [is-spam (m/check_is_spam reply-text)
-          notify-admin-fx (notify-admin reply-from-id is-spam chat-name reply-message-id reply-text)
+          notify-important-fx (notify-important reply-from-id is-spam chat-name reply-message-id reply-text)
           action-effects (cond
                            (m/is_too_old message-id reply-message-id)
-                           [notify-admin-fx
+                           [notify-important-fx
                             (tg/send_message :sendMessage
                                              {:chat_id chat-id
                                               :text (str "Репортить можно только " m/OLD_MESSAGE_ID_DIFF " последних сообщения. Администратор уведомлен.")})]
 
                            is-spam
                            [(tg/send_message :deleteMessage {:chat_id chat-id :message_id reply-message-id})
-                            (tg/send_message "restrictChatMember" {:chat_id chat-id :user_id reply-from-id :permissions {}})]
+                            (tg/send_message :restrictChatMember {:chat_id chat-id :user_id reply-from-id :permissions {}})]
 
                            :else
-                           [notify-admin-fx
+                           [notify-important-fx
                             (tg/send_message :sendMessage
                                              {:chat_id chat-id
                                               :text "Ваша жалоба принята, администратор оповещен."})])]
       (e/batch
-       (concat [(log-to-channel is-spam chat-name reply-message-id)
+       (concat [(notify is-spam chat-name reply-message-id reply-from-id)
                 (tg/send_message :deleteMessage {:chat_id chat-id :message_id message-id})]
                action-effects)))
     (e/pure nil)))
