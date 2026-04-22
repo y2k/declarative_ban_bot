de0cd8ecbd3799e657950e112f9cd37822de8af3
y2k <itwisterlx@gmail.com>
2026-04-22 17:49:29 +0200

    refactor: simplify build config

--- build.clj
+++ build.clj
@@ -2,28 +2,7 @@
 
 (deps {:make "0.5.0"})
 
-(def- prelude ".github/bin/src/prelude.js")
-(def- out-src ".github/bin/src")
-(def- out-test ".github/bin/test")
-
-(def- effects-promise
-  {:target "dep"
-   :name "effects-promise"
-   :version "0.3.0"
-   :compile_target "js"
-   :prelude-path prelude})
-
-(m/build
- {:rules
-  [{:target "dep"
-    :name "prelude"
-    :version "1.0.0/js"
-    :compile_target "js"
-    :out-dir out-src}
-   ;; Dependencies
-   (assoc effects-promise :out-dir out-src)
-   (assoc effects-promise :out-dir out-test)
-   ;; Sources
-   {:target "js" :root "src" :prelude-path prelude :out-dir out-src}
-   {:target "js" :root "src" :prelude-path prelude :out-dir out-test}
-   {:target "js" :root "test" :prelude-path prelude :out-dir out-test}]})
+(m/build-simple
+ {:out ".github/bin"
+  :target "js"
+  :deps [["effects-promise" "0.3.0"]]})
